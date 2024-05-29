5e1db3f98efd7fc6d3cc7bf314b3bb5f1dd640a3
BuzzardDev <hhackbeast81@gmai.com>
2024-05-29 12:29:44 -0700

    init commit

---
+++ movies/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context = {
+        'movies': ['gladiator', 'top gun', 'casino']
+    }
+    return render(request, 'movies/index.html', context)
+
+def about(request):
+    return render(request, 'movies/about.html', {})
